f60df8746b1e4a2e04365483971117893cca6731
Jess Varghese <jessvarghese@gmail.com>
2022-02-22 17:34:38 -0500

    fixed homepage path

--- src/components/Topnav/index.js
+++ src/components/Topnav/index.js
@@ -26,7 +26,7 @@ function TopNav() {
     <Container>
     <Navbar.Brand as={Link} to='/'>JMV</Navbar.Brand>
     <Nav className="ms-auto">
-      <Nav.Link as={Link} to=''>Home</Nav.Link>
+      <Nav.Link as={Link} to='/'>Home</Nav.Link>
       <Nav.Link as={Link} to='/about'>About</Nav.Link>
       <Nav.Link as={Link} to='/portfolio'>Portfolio</Nav.Link>
       <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
