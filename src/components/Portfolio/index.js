62f2ac1ffff7f81a427c4b764bab7f2a788d7150
Jess Varghese <jessvarghese@gmail.com>
2022-03-30 13:44:55 -0400

    updated portfolio page to add new card layout and updated projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Row, Col, Container } from "react-bootstrap";
+import { Card, Row, Col, Container, CardGroup } from "react-bootstrap";
 
 import ProjOne from "../../images/unsplash_img_cd.jpg";
 import ProjTwo from "../../images/catmom.jpg";
@@ -8,238 +8,231 @@ import Wedash from "../../images/weather_dashboard_thumb.png";
 import CommUnity from "../../images/common_unity.png";
 import ReadmeGen from "../../images/readme_gen.png";
 import PassGen from "../../images/password_gen.png";
+import Thankamma from "../../images/tk_homepage.png";
 import { Animated } from "react-animated-css";
+import { Button } from "bootstrap";
 
 function Portfolio() {
   return (
     <Animated animationIn="fadeInUpBig" animationOut="fadeOut" isVisible={true}>
-      <Container>
-        <Row lg={3}>
-          {/* <Col xs={3}> */}
-            {/* <Card className="p-3">
-              <a
-                href="https://limitless-sands-09171.herokuapp.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Card.Img variant="top" src={ProjThree} />
-                <Card.Body>
-                  <Card.Title>Gamers Garage (Currently In Development)</Card.Title>
-                </Card.Body>
+      <CardGroup>
+        <Card>
+          <a href="https://jessvarghese.github.io/Thankammas-Kitchen/" target="_blank" rel="noopener noreferrer">
+            <Card.Img variant="top" src={Thankamma} />
+          </a>
+          <Card.Body>
+            <Card.Title>
+              <a href="https://jessvarghese.github.io/Thankammas-Kitchen/" target="_blank" rel="noopener noreferrer">
+                Thankamma's Kitchen
               </a>
+            </Card.Title>
+            <Card.Text>
+              Thankamma's Kitchen is a fictional restaurant multi-page website
+              intended for the user to view a new up-and-coming restaurant. The
+              user can view visit info, contact information, menu, and can
+              provide their email address using the modal to stay up to date on
+              restaurant news.
+              <br></br>
+              <br></br>
               <Card.Subtitle>
-                Technologies: MERN Stack , GraphQL, RAWG Video Games API
+                Technologies: HTML, CSS, jQuery and Bootstrap
               </Card.Subtitle>
-
-              <Card.Text className="mt-3">
-                Gamers Garage is an application for the user who is interested
-                in browsing a database of video games they might be interested
-                in playing.
-              </Card.Text>
-
+            </Card.Text>
+            <div class="p-3">
               <a
-                href="https://github.com/Chis517/gamers-garage"
+                href="https://github.com/JessVarghese/Thankammas-Kitchen"
                 target="_blank"
-                rel="noopener noreferrer"
+                title="GitHub"
               >
-                <Card.Subtitle className="mt-3">Github Link</Card.Subtitle>
+                <i class="fa fa-github fa-3x"></i>
               </a>
-            </Card>
-          </Col> */}
+            </div>
+          </Card.Body>
+        </Card>
 
-          <Col xs={3}>
-            <Card className="p-3">
-              <a
-                href="https://jessvarghese.github.io/Pet-Project/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Card.Img variant="top" src={ProjOne} />
-                <Card.Body>
-                  <Card.Title>The Pet Project</Card.Title>
-                </Card.Body>
-              </a>
+        
+        <Card>
+        <a href="https://jessvarghese.github.io/Pet-Project/" target="_blank" rel="noopener noreferrer">
+          <Card.Img variant="top" src={ProjOne} />
+          </a>
+          <Card.Body>
+          <a href="https://jessvarghese.github.io/Pet-Project/" target="_blank" rel="noopener noreferrer">
+              <Card.Title>The Pet Project</Card.Title>
+            </a>
+            <Card.Text>
+              The Pet Project is a simple mobile-first application that allows a
+              user to easily find a dog or cat to potentially adopt. he user can
+              cheese with a dog or cat to search for, as well as input their zip
+              code to view the distance between their location and the pet's
+              location.
+              <br></br>
+              <br></br>
               <Card.Subtitle>
                 Technologies: Bulma, RescueGroups.org API , ZipAPI.us
               </Card.Subtitle>
-
-              <Card.Text className="mt-3">
-                The Pet Project is a simple mobile-first application that allows
-                a user to easily find a dog or cat to potentially adopt. he user
-                can cheese with a dog or cat to search for, as well as input
-                their zip code to view the distance between their location and
-                the pet's location.
-              </Card.Text>
-
+            </Card.Text>
+            <div class="p-3">
               <a
-                href="https://jessvarghese.github.io/Pet-Project/"
+                href="https://github.com/JessVarghese/Pet-Project"
                 target="_blank"
+                title="GitHub"
                 rel="noopener noreferrer"
               >
-                <Card.Subtitle className="mt-3">Github Link</Card.Subtitle>
+                <i class="fa fa-github fa-3x"></i>
               </a>
-            </Card>
-          </Col>
+            </div>
+          </Card.Body>
+        </Card>
 
-          <Col xs={2}>
-            <Card className="p-3">
-              <a
-                href="https://glacial-plains-94450.herokuapp.com/login"
+        <Card>
+        <a href="https://glacial-plains-94450.herokuapp.com/login"
                 target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Card.Img variant="top" src={ProjTwo} />
-                <Card.Body>
-                  <Card.Title>Cat Mom Dot Com</Card.Title>
-                </Card.Body>
-              </a>
-              <Card.Subtitle>
+                rel="noopener noreferrer">
+          <Card.Img variant="top" src={ProjTwo} />
+          </a>
+          <Card.Body>
+          <a href="https://glacial-plains-94450.herokuapp.com/login"
+                target="_blank"
+                rel="noopener noreferrer">
+
+            <Card.Title>Cat Mom Dot Com</Card.Title>
+            </a>
+            <Card.Text>
+            Who doesn’t love GIFs of cute cuddly cats? CatMom DotCom is a
+                site intended for a user to login to view random cat GIFs, facts
+                and get specific info on a particular breed of cat.
+
+                <br></br>
+              <br></br>
+                <Card.Subtitle>
                 Technologies: Bcrypt, cookie parser, dotenv, express,
                 express-handlebars, express session, handlebars, morgan, mysql2,
                 requirejs, sequelize, HTML, CSS, BULMA, Boostrap, Javascript
               </Card.Subtitle>
-              <Card.Text className="mt-3">
-                Who doesn’t love GIFs of cute cuddly cats? CatMom DotCom is a
-                site intended for a user to login to view random cat GIFs, facts
-                and get specific info on a particular breed of cat.
-              </Card.Text>
+            </Card.Text>
+            <div class="p-3">
               <a
                 href="https://github.com/Chis517/cat-mom-dotcom"
                 target="_blank"
-                rel="noopener noreferrer"
+                title="GitHub"
               >
-                <Card.Subtitle className="mt-3">Github Link</Card.Subtitle>
+                <i class="fa fa-github fa-3x"></i>
               </a>
-            </Card>
-          </Col>
+            </div>
+          </Card.Body>
+          
+        </Card>
+      </CardGroup>
 
-          <Col xs={4}>
-            <Card className="p-3">
-              <a
-                href="https://github.com/JessVarghese/common-unity-network"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Card.Img variant="top" src={CommUnity} />
-                <Card.Body>
-                  <Card.Title>Common Unity Network</Card.Title>
-                </Card.Body>
+
+
+      <CardGroup>
+        <Card>
+          <a href="https://jessvarghese.github.io/password-generator/" target="_blank"
+                rel="noopener noreferrer">
+            <Card.Img variant="top" src={PassGen} />
+          </a>
+          <Card.Body>
+            <Card.Title>
+              <a href="https://jessvarghese.github.io/password-generator/" target="_blank"
+                rel="noopener noreferrer">
+              Password Generator
               </a>
-              <Card.Subtitle>
-                Technologies: Express, Mongoose, Insomnia
-              </Card.Subtitle>
-
-              <Card.Text className="mt-3">
-                The Common Unity Network is a social application using a NoSQL
-                database where you can create users, add friends, share thoughts
-                and reactions.
-              </Card.Text>
-
-              <a
-                href="https://github.com/JessVarghese/common-unity-network"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Card.Subtitle className="mt-3">Github Link</Card.Subtitle>
-              </a>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-
-      {/* Second row of projects */}
-
-      <Container>
-        <Row>
-          <Col xs={4}>
-            <Card className="p-3">
-              <a
-                href="https://github.com/JessVarghese/Rocket-README#readme"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Card.Img variant="top" src={ReadmeGen} />
-                <Card.Body>
-                  <Card.Title>Rocket README</Card.Title>
-                </Card.Body>
-              </a>
-
-              <Card.Subtitle>
-                Technologies: HTML, Javascript, Inquirer
-              </Card.Subtitle>
-              <Card.Text className="mt-3">
-                The Rocket README is a command line application that utilizes
-                the inquirer package to generate a README file based on the
-                users criteria.
-              </Card.Text>
-              <a
-                href="https://github.com/JessVarghese/Rocket-README"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Card.Subtitle className="mt-3">Github Link</Card.Subtitle>
-              </a>
-            </Card>
-          </Col>
-
-          <Col xs={4}>
-            <Card className="p-3">
-              <a
-                href="https://jessvarghese.github.io/password-generator/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Card.Img variant="top" src={PassGen} />
-                <Card.Body>
-                  <Card.Title>Password Generator</Card.Title>
-                </Card.Body>
-              </a>
-              <Card.Subtitle>Technologies: HTML, Javascript</Card.Subtitle>
-              <Card.Text className="mt-3">
-                This application generates a random secure password based on the
+            </Card.Title>
+            <Card.Text>
+            This application generates a random secure password based on the
                 user's selected criteria.
-              </Card.Text>
+
+              <br></br>
+              <br></br>
+              <Card.Subtitle>
+              jQuery, Moment.js, BULMA, HTML, Javascript
+              </Card.Subtitle>
+            </Card.Text>
+            <div class="p-3">
               <a
                 href="https://github.com/JessVarghese/password-generator"
                 target="_blank"
+                title="GitHub"
                 rel="noopener noreferrer"
               >
-                <Card.Subtitle className="mt-3">Github Link</Card.Subtitle>
+                <i class="fa fa-github fa-3x"></i>
               </a>
-            </Card>
-          </Col>
+            </div>
+          </Card.Body>
+        </Card>
 
-          <Col xs={4}>
-            <Card className="p-3">
-              <a
-                href="https://jessvarghese.github.io/weather_dashboard/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Card.Img variant="top" src={Wedash} />
-                <Card.Body>
-                  <Card.Title>Weather Dashboard</Card.Title>
-                </Card.Body>
-              </a>
-              <Card.Subtitle>
-                Technologies: jQuery, Moment.js, BULMA, HTML, Javascript
-              </Card.Subtitle>
-              <Card.Text className="mt-3">
-                This application provides the user a way to easily search a city
+        
+        <Card>
+        <a href="https://jessvarghese.github.io/weather_dashboard/" target="_blank"
+                rel="noopener noreferrer">
+          <Card.Img variant="top" src={Wedash} />
+          </a>
+          <Card.Body>
+            <a href="https://jessvarghese.github.io/weather_dashboard/" target="_blank">
+              <Card.Title>Weather Dashboard</Card.Title>
+            </a>
+            <Card.Text>
+            This application provides the user a way to easily search a city
                 and have the current and five-day weather forecast displayed on
                 the dashboard.
-              </Card.Text>
+              <br></br>
+              <br></br>
+              <Card.Subtitle>
+              Technologies: jQuery, Moment.js, BULMA, HTML, Javascript
+              </Card.Subtitle>
+            </Card.Text>
+            <div class="p-3">
               <a
                 href="https://github.com/JessVarghese/weather_dashboard"
                 target="_blank"
-                rel="noopener noreferrer"
+                title="GitHub"
               >
-                <Card.Subtitle className="mt-3">Github Link</Card.Subtitle>
+                <i class="fa fa-github fa-3x"></i>
               </a>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+            </div>
+          </Card.Body>
+        </Card>
+
+        <Card>
+        <a href="https://github.com/JessVarghese/common-unity-network"
+                target="_blank"
+                rel="noopener noreferrer">
+          <Card.Img variant="top" src={CommUnity} />
+          </a>
+          <Card.Body>
+          <a href="https://github.com/JessVarghese/common-unity-network"
+                target="_blank"
+                rel="noopener noreferrer">
+            <Card.Title>Common Unity Network</Card.Title>
+            </a>
+            <Card.Text>
+            Who doesn’t love GIFs of cute cuddly cats? CatMom DotCom is a
+                site intended for a user to login to view random cat GIFs, facts
+                and get specific info on a particular breed of cat.
+
+                <br></br>
+              <br></br>
+                <Card.Subtitle>
+                Technologies: Express, Mongoose, Insomnia
+              </Card.Subtitle>
+            </Card.Text>
+            <div class="p-3">
+              <a
+                href="https://github.com/JessVarghese/common-unity-network"
+                target="_blank"
+                title="GitHub"
+              >
+                <i class="fa fa-github fa-3x"></i>
+              </a>
+            </div>
+          </Card.Body>
+          
+        </Card>
+      </CardGroup>
+
+
+
     </Animated>
   );
 }
